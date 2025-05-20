c7fd1281676a6958f45852c068344c881d3be01e
LonDev <dev@lenkavets.ru>
2025-05-20 14:49:52 +0300

    refactor: i am rewrite this part of code

--- currency-converter/src/components/CurrencyConverter.jsx
+++ currency-converter/src/components/CurrencyConverter.jsx
@@ -1,78 +1,82 @@
 import React, { useState } from "react";
 
-function CurrencyConverter ({rates}){
-    const [amount, setAmount] = useState(0);
-    const [fromCurrency, setFromCurrency] = useState('BYN');
-    const [toCurrency, setToCurrency] = useState('USD');
-    const [result,setResult] = useState(0);
+function CurrencyConverter({ rates }) {
+  const [amount, setAmount] = useState(0);
+  const [fromCurrency, setFromCurrency] = useState("BYN");
+  const [toCurrency, setToCurrency] = useState("USD");
+  const [result, setResult] = useState(0);
 
-    const convert = (amount, fromCurrency, toCurrency, rates) =>{
-        if(fromCurrency === 'BYN'){
-            return amount * rates[toCurrency];
-        } else if (toCurrency === 'BYN'){
-            return amount / rates[fromCurrency];
-        }
-        else{
-            return (amount/ rates[fromCurrency]) *rates[toCurrency]
-        }
+  const convert = (amount, fromCurrency, toCurrency, rates) => {
+    if (fromCurrency === "BYN") {
+      return amount * rates[toCurrency];
+    } else if (toCurrency === "BYN") {
+      return amount / rates[fromCurrency];
+    } else {
+      return (amount / rates[fromCurrency]) * rates[toCurrency];
     }
+  };
 
-    const handleAmountChange = (e)=>{
-        setAmount(e.target.value);
-    };
+  // Обработчик для input
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value);
+  };
 
-    const handleFromCurrencyChange = (e) =>{
-        setFromCurrency(e.target.value);
-    };
+  // Обработчики для select
+  const handleFromCurrencyChange = (e) => {
+    setFromCurrency(e.target.value);
+  };
 
-    const handleToCurrencyChange = (e) =>{
-        setToCurrency(e.target.value);
-    }
+  const handleToCurrencyChange = (e) => {
+    setToCurrency(e.target.value);
+  };
 
-    console.log(amount);
-     return(
-        <div>
-        <div className="converter-form container mt-5">
-        <h1>Конвертер валют</h1>
-        <div className="row">
-            <div className="col-md-6">
-            <input type="number" 
-            className="form-control" 
+  console.log(amount);
+  return (
+    <div className="converter-form container mt-5">
+      <h1>Конвертер валют</h1>
+      <div className="row">
+        <div className="col-md-6">
+          <input
+            type="number"
+            className="form-control"
             placeholder="Введите сумму"
             value={amount}
-            onChange={handleAmountChange} />
-            </div>
-            <div className="col-md-3">
-            <select 
+            onChange={handleAmountChange}
+          />
+        </div>
+        <div className="col-md-3">
+          <select
             className="form-select"
             value={fromCurrency}
-            onChange={handleToCurrencyChange}>
-                <option>BYN</option>
-                <option>USD</option>
-                <option>EUR</option>
-                <option>RUB</option>
-                <option>PLN</option>
-            </select>
-            </div>
-            <div className="col-md-3">
-            <select 
+            onChange={handleFromCurrencyChange}
+          >
+            <option value="BYN">BYN</option>
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
+            <option value="RUB">RUB</option>
+            <option value="PLN">PLN</option>
+          </select>
+        </div>
+        <div className="col-md-3">
+          <select
             className="form-select"
             value={toCurrency}
-            onChange={handleToCurrencyChange}
-            >
-                <option>USD</option>
-                <option>EUR</option>
-                <option>RUB</option>
-                <option>PLN</option>
-                <option>BYN</option>
-            </select>
-            </div>
+            onChange={handleToCurrencyChange}>
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
+            <option value="RUB">RUB</option>
+            <option value="PLN">PLN</option>
+            <option value="BYN">BYN</option>
+          </select>
         </div>
-        <div className="mt-3">
-            <h3>Результат: <span>{result}</span></h3>
-        </div>
-        </div>            
-        </div>
-    )
+      </div>
+      <div className="mt-3">
+        <h3>
+          Результат: <span>{amount}</span>
+        </h3>
+      </div>
+    </div>
+  );
 }
+
 export default CurrencyConverter;
