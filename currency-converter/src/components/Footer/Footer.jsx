bd207ce5e42dc1d98b0e8980beadf306117231de
LonDev <dev@lenkavets.ru>
2025-05-22 15:20:36 +0300

    fix: convert function

--- currency-converter/src/components/Footer/Footer.jsx
+++ currency-converter/src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 
 function Footer() {
   return (
-    <footer style={{ textAlign: "center", padding: "10px 0", background: "#f5f5f5" }}>
+    <footer className="footer" style={{ textAlign: "center", padding: "10px 0", background: "#f5f5f5" }}>
       © {new Date().getFullYear()} Все права защищены
     </footer>
   );
